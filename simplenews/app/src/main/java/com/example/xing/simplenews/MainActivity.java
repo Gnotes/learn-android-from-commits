9845f5af0c15a6d0521c777c92bad75a50e90f40
xing.he <1245036094@qq.com>
2018-06-28 17:24:39 +0800

    commit: 添加注释

--- simplenews/app/src/main/java/com/example/xing/simplenews/MainActivity.java
+++ simplenews/app/src/main/java/com/example/xing/simplenews/MainActivity.java
@@ -11,30 +11,50 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * 创建活动时的生命周期
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 加载布局
         setContentView(R.layout.activity_main);
+        // 获取Toolbar实例
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // 设置工具栏
         setSupportActionBar(toolbar);
-
+        // 获取悬浮按钮实例
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        // 设置悬浮按钮点击事件
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // 一个类似Toast的轻量级提示工具，能够与用户进行简单交互
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
     }
 
+    /**
+     * 创建顶部右边菜单
+     * @param menu
+     * @return
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+        // 加载布局，初始化菜单项
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
+    /**
+     * 菜单项点击时执行函数
+     * @param item
+     * @return
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
